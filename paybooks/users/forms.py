df925515b759d732660416a63581225cbeb4ae12
sirichunduri <chundurimounica@gmail.com>
2019-08-06 19:14:01 +0530

    added email as mandatory

--- paybooks/users/forms.py
+++ paybooks/users/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-	#email = forms.EmailFeild()
-	
-	class meta:
-		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
